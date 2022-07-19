ac582483daf1491e6b12a9dd9db80cd5a8dcf4e0
adharshcb <adharshcheloor98@gmail.com>
2022-07-19 16:27:49 +0530

    second commit

--- proj_bitstore/settings.py
+++ proj_bitstore/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'cart.apps.CartConfig',
     'orders.apps.OrdersConfig',
     'wishlist.apps.WishlistConfig',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -92,9 +93,10 @@ DATABASES = {
     'default': {
         'ENGINE':config('ENGINE'),
         'NAME':config('NAME'),
-        'USER':'postgres',
+        'USER':config('PG_USER'),
         'PASSWORD':config('PASSWORD'),
-        'HOST':config('HOST',default='localhost')
+        'HOST':config('HOST',default='localhost'),
+        'PORT':config('PORT',cast=int),
     }
 }
 
@@ -134,19 +136,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR /'static'
-STATICFILES_DIRS = [
-    'proj_bitstore/static',
-]
+# STATIC_ROOT = BASE_DIR /'static'
+# STATICFILES_DIRS = [
+#     'proj_bitstore/static',
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# media files configuration
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# # media files configuration
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # messages
@@ -168,3 +170,14 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS',cast=bool)
 #Payment Gateway 
 KEY = config('KEY')
 PAY_SECRET_KEY = config('PAY_SECRET_KEY')
+
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_FILE_OVERWRITE = True
+AWS_DEFAULT_ACL = None
